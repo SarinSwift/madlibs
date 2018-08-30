74e1468d66b902fd62adb5c13d523eaa7fe7c274
SarinSwift <sarinyaswift@gmail.com>
2018-08-29 19:27:13 -0700

    Started the madlibs project

---
+++ madLib.py
@@ -0,0 +1,16 @@
+# MadLibs
+
+Sentence = "The <noun> was invented by <person> who worked very <adj> and <adj> to create it. The masterpiece was used across <number> people!"
+
+input: 1 noun
+        1 person
+        2 adjectives
+        1 number
+
+output: The complete sentence including the users inputs
+
+process: noun_1()
+        person_1()
+        adj_1()
+        adj_2()
+        num_1()
